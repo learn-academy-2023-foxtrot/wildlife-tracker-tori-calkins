a1459d8ad120b30e206bf7ff9d559c0dc5663446
tori-calkins <tori.calkins2705@gmail.com>
2023-10-06 10:26:00 -0700

    Finished story 1

--- app/controllers/wildlife_trackers_controller.rb
+++ app/controllers/wildlife_trackers_controller.rb
@@ -1,17 +1,39 @@
 class WildlifeTrackersController < ApplicationController
     def index
-        animal = WildlifeTracker.all
+        animals = WildlifeTracker.all
+        render json: animals
+    end
+    def show
+        animal = WildlifeTracker.find(params[:id])
         render json: animal
     end
     def create
         animal = WildlifeTracker.create(animal_params)
         if animal.valid?
             render json: animal
-        else render json: animal.errors
+        else 
+            render json: animal.errors
+        end
+    end
+    def update
+        animal = WildlifeTracker.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
+            render json: animal
+        else 
+            render json: animal.errors
+        end
+    end
+    def destroy
+        animal = WildlifeTracker.find(params[:id])
+        if animal.destroy
+            render json: animal
+        else 
+            render json: animal.errors
         end
     end
     private
     def animal_params
-        params.require(:animal).permit(:common_name, :scientific_binomial)
+        params.require(:wildlife_tracker).permit(:common_name, :scientific_binomial)
     end
 end
